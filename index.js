efffeab6e0aea5294e62d97557e521bcdb412845
ToastyStudent <kanelayng2023@u.northwestern.edu>
2023-07-03 22:44:09 -0500

    Added a comment in the index.js file

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+// This code is necessary in order to the run the logic of the program, which is defined in the cli.js file as node index.js
+// is how the program is run.
 const CLI = require("./lib/cli");
 
 new CLI().run();
